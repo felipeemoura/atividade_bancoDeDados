a80a7de7b85688ae601c84c60b3a8e3dbe448f79
kynsh <feliphsantoos@gmail.com>
2021-12-06 21:20:07 -0300

    new API

---
+++ src/app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const app = express();
+
+
+mongoose.connect("mongodb://localhost27017", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+let db = mongoose.connection;
+
+db.on("error", console.log.bind(console, "connection error:"));
+db.once("open", function () {
+    console.log("conexão feita com sucesso.")
+});
+
+
+const index = require("./routes/index");
+const tarefas = require("./routes/tarefasRoute");
+
+
+app.use(bodyParser.json());
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    )
+    next()
+});
+
+app.use("/", index);
+app.use("/tarefas", tarefas);
+
+module.exports = app;
